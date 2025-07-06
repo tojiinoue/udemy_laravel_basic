9d7495e674271ebad83934b4b1fcb3a910622993
Touji Inoue <touinoue@X-Tech-0670001.local>
2025-07-06 16:05:35 +0900

    first

---
+++ app/Models/ContactForm.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContactForm extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'title',
+        'email',
+        'url',
+        'gender',
+        'age',
+        'contact',
+    ];
+    public function scopeSearch($query, $search){
+
+    if($search !== null){
+        $search_split = mb_convert_kana($search, 's');
+        $search_split2 = preg_split('/[\s]+/', $search_split);
+        foreach( $search_split2 as $value ){
+            $query->where('name', 'like', '%' .$value. '%');}
+    }
+    return $query;
+    }
+}
